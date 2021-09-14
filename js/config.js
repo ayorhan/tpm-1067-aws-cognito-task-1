bbee30da05e27f82adb2a3b3c23998cc0bb2744d
Arda Orhan <arda.orhan@devfactory.com>
2021-09-14 15:58:15 +0300

    updated client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-1_GwoKjhjVT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '17api2076d2p1gtuk4t6h9u95d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '49ebmui8ej5sfci88scpnfagak', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
