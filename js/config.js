4e483e423c85e4dcfc169a39a268dc6b0658a442
Arda Yigithan Orhan <ayorhan@ip-192-168-8-119.ec2.internal>
2021-09-14 15:52:22 +0300

    updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_GwoKjhjVT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17api2076d2p1gtuk4t6h9u95d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://main.d2qd6nrjiw3kov.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
